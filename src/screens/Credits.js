e8e5c4fc8518a0c2a76cdbc6913af6a99d102021
Pierre29G <pierre29g@gmail.com>
2022-11-18 12:37:55 +0100

    Changement du contenu texte Credits.js

--- src/screens/Credits.js
+++ src/screens/Credits.js
@@ -1,112 +1,33 @@
-import * as React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import theme from '../../assets/style/theme';
-import { useQuery,QueryClient,QueryClientProvider,} from '@tanstack/react-query'
+import * as React from 'react'
+import { Text, ScrollView } from 'react-native'
+import theme from '../../assets/style/theme'
 
-function Home({ navigation }) {
-  const dateoptions = { weekday: 'short' };
-  
-  const { isLoading, error, data } = useQuery(['repoData'], () =>
-    fetch('http://weather-api.mathisbarre.com/nantes').then(res =>
-      {
-        if(!res.ok){
-          throw new Error('Fail');
-        }
-      return res.json()}
-      )
-  )
-
-  const wait = (timeout) => {
-    return new Promise(resolve => setTimeout(resolve, timeout));
-  }
-  
-  const [refreshing, setRefreshing] = React.useState(false);
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
-    
-    if (isLoading){
-        return (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>
-              Loading...
-            </Text>
-          </View>
-      );
-    }
-
-    if (error){
-        return (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>
-              Something went wrong !
-            </Text>
-          </View>
-      );
-    }
-
-    function TextHeadingSmall({wanteddata}){
-      return (
-        <View style={theme.column}>
-          <Text style={theme.s}>
-            {data?.currentConditions[wanteddata]['value']} {data?.currentConditions[wanteddata]['unit']}
-          </Text>
-          <Text style={theme.xs}>
-            Vent
-          </Text>
-        </View>
-      )
-    }
-    
+function Credits () {
   return (
-      <ScrollView style={theme.general} 
-                    contentContainerStyle={theme.scrollView}
-                      refreshControl={
-                        <RefreshControl
-                          refreshing={refreshing}
-                          onRefresh={onRefresh}
-                        />
-                      }>
-        <View style={theme.topcontainer}>
-          <View style={theme.row}>
-            <Image source={{uri: data.currentConditions.iconBig}} style={theme.thumbnail} />
-            <View style={theme.column}>
-              <Text style={theme.surtitle}>
-                Actuellement
-              </Text>
-              <Text style={theme.title}>
-                {data?.currentConditions.temperature['value']} {data?.currentConditions.temperature['unit']}
-              </Text>
-            </View>
-          </View>
-          <View style={theme.row}>
-            <TextHeadingSmall wanteddata='windSpeed'/>
-            <TextHeadingSmall wanteddata='humidity'/>
-          </View>
-        </View>
-        <View style={theme.padtop}>
-          {data.next5DaysConditions.map(day => 
-          <TouchableOpacity onPress={() => navigation.navigate('Day', {daydate: day?.date})} style={theme.dayview}>
+      <ScrollView style={theme.general}>
             <Text style={theme.m}>
-              {new Date(day?.date).toLocaleDateString('fr-FR', dateoptions)}
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀
+            ⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀
+            ⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀
+            ⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀
+            ⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀
+            ⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀
+            ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+            ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+            ⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀
+            ⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀
+            ⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
             </Text>
-            <Image source={{uri: day?.icon}} style={theme.smallthumbnail} />
-            <Text style={theme.mdark}>
-              {day?.condition}
-            </Text>
-            <Text style={theme.m}>
-              Max. {day?.temperature.max} {day?.temperature.unit}
-            </Text>
-          </TouchableOpacity>
-          )}
-        </View >
       </ScrollView>
-  );
+  )
 }
 
-export default Home;
+export default Credits
