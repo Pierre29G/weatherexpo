4da1556b79ea12f5f83f1e6f5576e8ac5f2f8acc
Pierre29G <pierre29g@gmail.com>
2022-11-18 12:12:20 +0100

    Création StackApp.js

---
+++ src/navigation/StackApp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import Day from '../screens/Day';
+
+const Stack = createNativeStackNavigator();
+
+function StackApp() {
+
+  return (
+        <Stack.Navigator
+        screenOptions={{
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: '#1b212f' },
+        }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Day" component={Day} />
+        </Stack.Navigator>
+  )
+}
+
+export default StackApp;
