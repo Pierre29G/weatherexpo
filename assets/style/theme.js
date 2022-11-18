2f14fdb6e9c8c22fca05a4c3d2fd6653de4a95a7
Pierre29G <pierre29g@gmail.com>
2022-11-18 12:11:46 +0100

    Création du thème

---
+++ assets/style/theme.js
@@ -0,0 +1,78 @@
+const theme = {
+    general: {
+        backgroundColor: '#000918',
+      },
+      topcontainer: {
+        marginBottom: 20,
+        paddingRight : 25,
+        paddingLeft : 25,
+        paddingTop : 25,
+        paddingBottom: 10,
+        height: 250,
+        backgroundColor: '#1b212f',
+        borderBottomLeftRadius: 50,
+        borderBottomRightRadius: 50,
+      },
+      dayview: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: 50,
+        marginBottom : 50,
+        marginRight : 20,
+        marginLeft : 20,
+        borderBottomColor: '#1b212f',
+        borderBottomWidth: 2
+      },
+      row: {
+        flex: 1,
+        flexDirection: "row",
+      },
+      column: {
+        flex: 1,
+        flexDirection: "column",
+        margin: 5,
+      },
+      thumbnail: {
+        width: 100,
+        height: 100,
+        marginRight: 10,
+        marginLeft: 30,
+      },
+      smallthumbnail: {
+        width: 25,
+        height: 25,
+        marginRight: 10,
+      },
+      surtitle: {
+        color: 'white',
+        fontSize: 25,
+      },
+      title: {
+        color: 'white',
+        fontSize: 30,
+      },
+      m: {
+        color: 'white',
+        fontSize: 20,
+      },
+      mdark: {
+        color: '#b4bac4',
+        fontSize: 20,
+      },
+      s: {
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center',
+      },
+      xs: {
+        color: '#b4bac4',
+        fontSize: 15,
+        textAlign: 'center',
+      },
+      padtop: {
+      paddingTop: 25,
+      },
+};
+
+export default theme;
