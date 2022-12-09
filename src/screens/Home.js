47f491facdaad947dfb2bcedd73ddb5d34c23551
Pierre29G <pierre29g@gmail.com>
2022-12-09 14:21:21 +0100

    Correction Eslint de home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,67 +1,67 @@
-import * as React from 'react';
-import { Image, Text, TouchableOpacity, View, ScrollView, RefreshControl } from 'react-native';
-import theme from '../../assets/style/theme';
-import { useQuery} from '@tanstack/react-query'
+import * as React from 'react'
+import { Image, Text, TouchableOpacity, View, ScrollView, RefreshControl } from 'react-native'
+import theme from '../../assets/style/theme'
+import { useQuery } from '@tanstack/react-query'
 
-function Home({ navigation }) {
-  const dateoptions = { weekday: 'short' };
-  
+function Home ({ navigation }) {
+  const dateoptions = { weekday: 'short' }
+
   const { isLoading, error, data } = useQuery(['repoData'], () =>
-    fetch('http://weather-api.mathisbarre.com/nantes').then(res =>
-      {
-        if(!res.ok){
-          throw new Error('Fail');
-        }
-      return res.json()}
-      )
+    fetch('http://weather-api.mathisbarre.com/nantes').then(res => {
+      if (!res.ok) {
+        throw new Error('Fail')
+      }
+      return res.json()
+    }
+    )
   )
 
   const wait = (timeout) => {
-    return new Promise(resolve => setTimeout(resolve, timeout));
+    return new Promise(resolve => setTimeout(resolve, timeout))
   }
-  
-  const [refreshing, setRefreshing] = React.useState(false);
+
+  const [refreshing, setRefreshing] = React.useState(false)
 
   const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
-    
-    if (isLoading){
-        return (
+    setRefreshing(true)
+    wait(2000).then(() => setRefreshing(false))
+  }, [])
+
+  if (isLoading) {
+    return (
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
               Loading...
             </Text>
           </View>
-      );
-    }
+    )
+  }
 
-    if (error){
-        return (
+  if (error) {
+    return (
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
               Something went wrong !
             </Text>
           </View>
-      );
-    }
+    )
+  }
 
-    function TextHeadingSmall({wanteddata}){
-      return (
+  function TextHeadingSmall ({ wanteddata }) {
+    return (
         <View style={theme.column}>
           <Text style={theme.s}>
-            {data?.currentConditions[wanteddata]['value']} {data?.currentConditions[wanteddata]['unit']}
+            {data?.currentConditions[wanteddata].value} {data?.currentConditions[wanteddata].unit}
           </Text>
           <Text style={theme.xs}>
             Vent
           </Text>
         </View>
-      )
-    }
-    
+    )
+  }
+
   return (
-      <ScrollView style={theme.general} 
+      <ScrollView style={theme.general}
                     contentContainerStyle={theme.scrollView}
                       refreshControl={
                         <RefreshControl
@@ -71,13 +71,13 @@ function Home({ navigation }) {
                       }>
         <View style={theme.topcontainer}>
           <View style={theme.row}>
-            <Image source={{uri: data.currentConditions.iconBig}} style={theme.thumbnail} />
+            <Image source={{ uri: data.currentConditions.iconBig }} style={theme.thumbnail} />
             <View style={theme.column}>
               <Text style={theme.surtitle}>
                 Actuellement
               </Text>
               <Text style={theme.title}>
-                {data?.currentConditions.temperature['value']} {data?.currentConditions.temperature['unit']}
+                {data?.currentConditions.temperature.value} {data?.currentConditions.temperature.unit}
               </Text>
             </View>
           </View>
@@ -87,12 +87,12 @@ function Home({ navigation }) {
           </View>
         </View>
         <View style={theme.padtop}>
-          {data.next5DaysConditions.map(day => 
-          <TouchableOpacity onPress={() => navigation.navigate('Day', {daydate: day?.date})} style={theme.dayview}>
+          {data.next5DaysConditions.map(day =>
+          <TouchableOpacity onPress={() => navigation.navigate('Day', { daydate: day?.date })} style={theme.dayview}>
             <Text style={theme.m}>
               {new Date(day?.date).toLocaleDateString('fr-FR', dateoptions)}
             </Text>
-            <Image source={{uri: day?.icon}} style={theme.smallthumbnail} />
+            <Image source={{ uri: day?.icon }} style={theme.smallthumbnail} />
             <Text style={theme.mdark}>
               {day?.condition}
             </Text>
@@ -103,7 +103,7 @@ function Home({ navigation }) {
           )}
         </View >
       </ScrollView>
-  );
+  )
 }
 
-export default Home;
+export default Home
